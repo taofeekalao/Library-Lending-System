3dd276b3b2a68d1ba58817985e8536720c0e2220
Taofeek Alao <taofeekalao@gmail.com>
2024-02-19 14:56:21 +0000

    Set up the initial code skeleton using SpringBoot Initializer
    Set up the initial code skeleton using SpringBoot Initializer.
    
    The dependencies added are Spring Web, Spring Boot Test, Spring HATEOAS, H2 Databse, Spring Boot DevTools, and Thymeleaf.

---
+++ src/main/java/cs5031/group/one/thelibrary/TheLibraryApplication.java
@@ -0,0 +1,13 @@
+package cs5031.group.one.thelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs5031/group/one/thelibrary/TheLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cs5031.group.one.thelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
