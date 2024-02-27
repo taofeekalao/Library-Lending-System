21aec6c839381f2bac7469190d5678136666a1df
Taofeek Alao <taofeekalao@gmail.com>
2024-02-27 10:40:05 +0000

    Auto-reformatted the codes.
    Auto-reformatted the codes.
    Added the quantity of a book in the library and the quantity of a copy checked out to the Book class.

--- src/main/java/cs5031/group/one/thelibrary/TheLibraryApplication.java
+++ src/main/java/cs5031/group/one/thelibrary/TheLibraryApplication.java
@@ -2,7 +2,6 @@ package cs5031.group.one.thelibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * This is the entry point to the SpringBoot application.
@@ -12,10 +11,10 @@ public class TheLibraryApplication {
 
     /**
      * This is the main method for the application.
+     *
      * @param args This is argument/parameter to the main method od the application.
      */
     public static void main(String[] args) {
         SpringApplication.run(TheLibraryApplication.class, args);
     }
-
 }
--- src/main/java/cs5031/group/one/thelibrary/model/Book.java
+++ src/main/java/cs5031/group/one/thelibrary/model/Book.java
@@ -4,11 +4,13 @@ import org.springframework.data.annotation.Id;
 
 /**
  * This is a record class representing the book item in the library.
- * @param bookId This is the unique id of the book item.
- * @param isbn This is the ISBN of the book item.
- * @param title This is the title of the book item.
- * @param author This is the author of the book item.
+ *
+ * @param bookId   This is the unique id of the book item.
+ * @param isbn     This is the ISBN of the book item.
+ * @param title    This is the title of the book item.
+ * @param author   This is the author of the book item.
  * @param quantity This is the quantity of the book item in the library.
  */
-public record Book(@Id Long bookId, String isbn, String title, String author, int quantity) {
+public record Book(@Id Long bookId, String isbn, String title, String author, int quantityInLibrary,
+                   int quantityCheckedOut) {
 }
