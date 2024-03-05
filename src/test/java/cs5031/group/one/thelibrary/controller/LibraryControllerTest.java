e15d41afa3c83ac5cb82a58c84d2b2f5f0017246
Taofeek Alao <taofeekalao@gmail.com>
2024-03-05 18:54:59 +0000

    Updated Java doc comments for member feature test cases and introduced test cases for member update functionality.
    Updated Java doc comments for member feature test cases and introduced test cases for member update functionality.

--- src/test/java/cs5031/group/one/thelibrary/controller/LibraryControllerTest.java
+++ src/test/java/cs5031/group/one/thelibrary/controller/LibraryControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -95,6 +96,9 @@ public class LibraryControllerTest {
         assertThat(createDuplicateResponse.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Test case to test the end-point for deleting a member using member's id.
+     */
     @Test
     @DirtiesContext
     void shouldDeleteAnExistingMember() {
@@ -103,6 +107,9 @@ public class LibraryControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * Test case to test the end-point to not delete a new that does not exist.
+     */
     @Test
     @DirtiesContext
     void shouldNotDeleteAMemberThatDoesNotExist() {
@@ -111,7 +118,9 @@ public class LibraryControllerTest {
         assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
-
+    /**
+     * Test case to test the end-point for deleting a member using member's email address.
+     */
     @Test
     @DirtiesContext
     void shouldDeleteAnExistingMemberByEmailAddress() {
@@ -120,6 +129,9 @@ public class LibraryControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * Test case to test the end-point for getting an existing member using member's id.
+     */
     @Test
     @DirtiesContext
     void shouldReturnAnExistingMember() {
@@ -128,6 +140,9 @@ public class LibraryControllerTest {
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    /**
+     * Test case to test the end-point for getting all members in the system.
+     */
     @Test
     @DirtiesContext
     void shouldReturnAllLibraryMembersWhenListIsRequested() {
@@ -141,5 +156,63 @@ public class LibraryControllerTest {
 
         JSONArray ids = documentContext.read("$..emailAddress");
         assertThat(ids).containsExactlyInAnyOrder("user.one@st-andrews.ac.uk", "user.two@st-andrews.ac.uk", "user.three@st-andrews.ac.uk", "user.four@st-andrews.ac.uk", "user.five@st-andrews.ac.uk");
+    }
+
+    /**
+     * Test case to test the end-point for updating a member's details (name and address).
+     */
+    @Test
+    @DirtiesContext
+    void shouldUpdateAnExistingMember() {
+        Member memberUpdate = new Member(null, "Mathew Lewis",  null, null);
+        Member memberUpdate1 = new Member(null, null, "Johnson S4 Fife Park, KY16 0US", null);
+        HttpEntity<Member> request = new HttpEntity<>(memberUpdate);
+        ResponseEntity<Void> response = restTemplate
+                .exchange("library/member/2", HttpMethod.PUT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate
+                .getForEntity("library/member/2", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("@.memberId");
+        String email = documentContext.read("@.emailAddress");
+
+        assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(2);
+        assertThat(email).isEqualTo("user.two@st-andrews.ac.uk");
+    }
+
+    /**
+     * Test case to test the end-point for updating a member's details (name and address).
+     * It should not update a member that does not exist
+     */
+    @Test
+    void shouldNotUpdateAMemberThatDoesNotExist() {
+        Member unknownMember = new Member(null, "Mathew Lewis",  null, null);
+        HttpEntity<Member> request = new HttpEntity<>(unknownMember);
+        ResponseEntity<Void> response = restTemplate
+                .exchange("library/member/99999", HttpMethod.PUT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Test case to test the end-point for updating a member's details (name and address).
+     * It should not update a member's email address.
+     */
+    @Test
+    void shouldNotUpdateEmailAddressOfAMember() {
+        Member memberWithEmailAddress = new Member(null, "Mathew Lewis",  null, "user.two@st-andrews.ac.uk");
+        HttpEntity<Member> request = new HttpEntity<>(memberWithEmailAddress);
+        ResponseEntity<Void> response = restTemplate
+                .exchange("library/member/2", HttpMethod.PUT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate
+                .getForEntity("library/member/2", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        String email = documentContext.read("@.emailAddress");
+        assertThat(email).isEqualTo("user.two@st-andrews.ac.uk");
     }
 }
